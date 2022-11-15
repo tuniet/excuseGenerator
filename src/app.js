2d0553e14a8319af44253753cefe67cee0b4ca8b
tuniet <tofeju@gmail.com>
2022-11-15 16:15:43 +0000

    remake

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuse =
+    who[getRandomInt(who.length - 1)] +
+    " " +
+    what[getRandomInt(what.length - 1)] +
+    " " +
+    "My homework" +
+    " " +
+    when[getRandomInt(when.length - 1)];
+  document.getElementById("excuse").innerHTML = excuse;
 };
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
